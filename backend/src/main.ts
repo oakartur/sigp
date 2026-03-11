2f60ec7d189020ddb77ba0f90f615372ddacd5b8
Artur Alves de Carvalho <oakartur@gmail.com>
2026-03-11 14:11:17 -0400

    updates host/network

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // Permite o Frontend (porta 8080) acessar o Backend (porta 3000)
-  await app.listen(process.env.PORT ?? 3000);
+  
+  // Garantir que a API escute em todas as interfaces do container (0.0.0.0)
+  // Caso contrário, ela pode ficar presa apenas no "localhost" interno do Docker.
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Permite acesso externo à rede do container
+    port: 80,
+    strictPort: true, // Garante que não irá procurar outra porta aleatória
+  }
 })
